57d473cc70f90adcb602a70af4d9bdeb83cff46c
Chris Short <chris.short323@gmail.com>
2025-05-21 10:51:49 -0500

    added sign in with apple to login screen

--- expo_tinderforfighters/screens/LoginScreen.js
+++ expo_tinderforfighters/screens/LoginScreen.js
@@ -1,12 +1,50 @@
 // This is a barebones screen component. Build out design, logic, and state as needed.
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import * as AppleAuthentication from 'expo-apple-authentication';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <Text>Login Screen</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      
+        <AppleAuthentication.AppleAuthenticationButton
+            buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+            buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+            cornerRadius={5}
+            style={styles.button}
+            onPress={async () => {
+                try {
+                    const credential = await AppleAuthentication.signInAsync({
+                        requestedScopes: [
+                            AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                            AppleAuthentication.AppleAuthenticationScope.EMAIL,
+                        ],
+                    });
+                // signed in
+                console.log(credential);
+                navigation.navigate('Home')
+                } catch (e) {
+                    if (e.code === 'ERR_REQUEST_CANCELED') {
+                        // handle that the user cancled sign in flow
+                    } else {
+                        // handle other errors
+                    }
+                }
+            }}
+            />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    width: 200,
+    height: 44,
+  },
+});
