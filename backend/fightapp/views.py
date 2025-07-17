2e3c2b94830b695e40f6d75923f7c82f89a354f8
Chris Short <chris.short323@gmail.com>
2025-07-17 12:01:34 -0500

    dd

--- backend/fightapp/views.py
+++ backend/fightapp/views.py
@@ -9,7 +9,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from datetime import timedelta
-from services import email_verification
+from backend.services import email_verification
 
 # send email verification code
 @api_view(['POST'])
